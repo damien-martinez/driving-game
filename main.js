979ed59e7531c331dc7313188f4d65821afbf0e4
Damien Martinez <mart.damien1@gmail.com>
2022-02-24 17:19:49 -0800

    Completed issue 3 Had incorrect branch name. Branch is to move car.

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-var $image = document.querySelector('.car');
+var $image = document.querySelector('.car-right');
+var shiftPressed = false;
+var intervalID;
 
-function test(event) {
+var topCount = 0;
+var leftCount = 0;
+
+function moveDirection(event) {
   if (event.key === 'ArrowUp') {
     $image.setAttribute('class', 'car-up');
   } else if (event.key === 'ArrowDown') {
@@ -12,7 +17,32 @@ function test(event) {
     $image.setAttribute('class', 'car-right');
 
   }
+  // console.log($image);
 
 }
 
-document.addEventListener('keydown', test);
+document.addEventListener('keydown', moveDirection);
+
+function listenForShift(event) {
+  if (event.code === 'Space') {
+    if (shiftPressed === false) {
+      intervalID = setInterval(moveCar, 16);
+      shiftPressed = true;
+    } else {
+      clearInterval(intervalID);
+      shiftPressed = false;
+    }
+  }
+}
+
+document.addEventListener('keypress', listenForShift);
+
+function moveCar(event) {
+  if ($image.getAttribute('class') === 'car-right') {
+    leftCount += 5;
+    $image.setAttribute('style', 'top:' + topCount.toString() + 'px; ' + 'left:' + leftCount.toString() + 'px');
+  }
+
+}
+
+/* <img style="top:0px; left:0px" class="car" src="images/f1.svg" alt=""></img> */
